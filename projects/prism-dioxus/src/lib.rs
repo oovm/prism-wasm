63dbda9165e69dd3cf2433b6fe1ee9114b0ba2cd
Aster <galaster@foxmail.com>
2022-03-08 23:42:38 +0800

    Define prism hook

--- projects/prism-dioxus/src/lib.rs
+++ projects/prism-dioxus/src/lib.rs
@@ -1,13 +1,14 @@
 #![forbid(missing_docs)]
 #![forbid(missing_debug_implementations)]
 #![forbid(rustdoc::missing_crate_level_docs)]
-#![doc = include_str!("../Readme.md")]
+#![doc = include_str ! ("../Readme.md")]
 #![doc(html_logo_url = "https://avatars.githubusercontent.com/u/31191489")]
 #![doc(html_favicon_url = "https://avatars.githubusercontent.com/u/31191489")]
 
-pub use self::katex::{
-    builder::{use_katex, use_katex_display, use_katex_inline},
+pub use self::prism::{
+    builder::{use_prism_rust, use_prism},
     UsePrism,
 };
-pub use prism_wasmbind::KaTeXOptions;
+pub use prism_wasmbind::*;
+
 mod prism;
--- projects/prism-dioxus/src/prism/builder.rs
+++ projects/prism-dioxus/src/prism/builder.rs
@@ -1,21 +1,17 @@
 use super::*;
 
-/// A builder for a [`UseKatex`] hook.
-pub fn use_katex(cx: &ScopeState, katex: KaTeXOptions) -> &mut UsePrism {
-    let katex = UsePrism { katex: Rc::new(RefCell::new(katex)), updater: cx.schedule_update() };
+/// A builder for a [`UsePrism`] hook.
+pub fn use_prism(cx: &ScopeState, prism: PrismOptions) -> &mut UsePrism {
+    let katex = UsePrism { prism: Rc::new(RefCell::new(prism)), updater: cx.schedule_update() };
     cx.use_hook(|_| katex)
 }
-/// A builder for a [`UseKatex`] hook in display mode.
-pub fn use_katex_display(cx: &ScopeState) -> &mut UsePrism {
-    use_katex(cx, KaTeXOptions::display_mode())
-}
-/// A builder for a [`UseKatex`] hook in inline mode.
-pub fn use_katex_inline(cx: &ScopeState) -> &mut UsePrism {
-    use_katex(cx, KaTeXOptions::inline_mode())
+/// A builder for a [`UsePrism`] hook in display mode.
+pub fn use_prism_rust(cx: &ScopeState) -> &mut UsePrism {
+    use_prism(cx, PrismOptions::rust())
 }
 
 impl Debug for UsePrism {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        Debug::fmt(&self.katex.borrow(), f)
+        Debug::fmt(&self.prism.borrow(), f)
     }
 }
